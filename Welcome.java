45dd620983a3950f7902365bd4abd9379aa28a76
Edri <edrianajoyrdamaso@iskolarngbayan.pup.edu.ph>
2025-01-16 14:40:04 +0800

    Draft for Welcome Page

---
+++ Welcome.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame; 
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+public class Welcome implements ActionListener {
+    
+    public static void main(String[] args) {
+        
+        JFrame f = new JFrame();
+        JButton b = new JButton ();
+
+        Welcome(){
+
+            
+
+        }
+    }
+}
